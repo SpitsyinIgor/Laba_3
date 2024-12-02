55736bde843711d48a70db20b83528392de44101
SpitsyinIgor <1overflash1@gmail.com>
2024-12-02 23:35:03 +0300

    Update ArrayListTest.java

--- laba_3/src/test/java/org/example/ArrayListTest.java
+++ laba_3/src/test/java/org/example/ArrayListTest.java
@@ -9,4 +9,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayListTest {
+   private List<Integer> linkedList;
+
+    @BeforeEach
+    public void setUp() {
+        linkedList = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(i);
+        }
+    }
+
+    @Test
+    public void whenAdded100ElementsThenSizeMustBe100() {
+        assertEquals(100, linkedList.size());
+    }
 }
