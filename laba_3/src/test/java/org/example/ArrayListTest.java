41435d55630525e2594aa30f518502a6aa6281bf
SpitsyinIgor <1overflash1@gmail.com>
2024-12-02 23:41:32 +0300

    Update ArrayListTest.java

--- laba_3/src/test/java/org/example/ArrayListTest.java
+++ laba_3/src/test/java/org/example/ArrayListTest.java
@@ -23,4 +23,18 @@ public class ArrayListTest {
     public void whenAdded100ElementsThenSizeMustBe100() {
         assertEquals(100, linkedList.size());
     }
+
+   @Test
+    public void whenElementRemovedByIndexThenSizeMustBeDecreased() {
+        linkedList.remove(5);
+        assertEquals(99, linkedList.size());
+    }
+
+    @Test
+    public void whenElementRemovedThenSizeMustBeDecreased() {
+        linkedList.add(102);
+        assertEquals(101, linkedList.size());
+        assertTrue(linkedList.remove(Integer.valueOf(102)));
+        assertEquals(100, linkedList.size());
+    }
 }
