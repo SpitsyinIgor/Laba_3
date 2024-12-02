1438caf67e14c784546ed374a1ba972cba5e303e
SpitsyinIgor <1overflash1@gmail.com>
2024-12-02 23:57:14 +0300

    Update LinkedListTest.java

--- laba_3/src/test/java/org/example/LinkedListTest.java
+++ laba_3/src/test/java/org/example/LinkedListTest.java
@@ -9,4 +9,32 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
+    private List<Integer> linkedList;
+
+    @BeforeEach
+    public void setUp() {
+        linkedList = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(i);
+        }
+    }
+
+    @Test
+    public void whenAdded100ElementsThenSizeMustBe100() {
+        assertEquals(100, linkedList.size());
+    }
+
+    @Test
+    public void whenElementRemovedByIndexThenSizeMustBeDecreased() {
+        linkedList.remove(5);
+        assertEquals(99, linkedList.size());
+    }
+
+    @Test
+    public void whenElementRemovedThenSizeMustBeDecreased() {
+        linkedList.add(102);
+        assertEquals(101, linkedList.size());
+        assertTrue(linkedList.remove(Integer.valueOf(102)));
+        assertEquals(100, linkedList.size());
+    }
 }
