b8e281696bdb5410597e29fd5aa7a43667a320f8
SpitsyinIgor <1overflash1@gmail.com>
2024-12-02 23:26:29 +0300

    Update LinkedListTest.java

---
+++ laba_3/src/test/java/org/example/LinkedListTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTest {
+}
